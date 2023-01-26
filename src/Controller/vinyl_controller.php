2b33307ed0fc56d28738fbd78d4fd23d0d0890c5
Ivan Turko <turko.ivan2021@gmail.com>
2023-01-26 19:35:14 +0100

    Adding some Twig stuff

--- src/Controller/vinyl_controller.php
+++ src/Controller/vinyl_controller.php
@@ -1,17 +1,27 @@
 <?php
 
 namespace App\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use function Symfony\Component\String\u;
 
-class vinyl_controller
+class vinyl_controller extends AbstractController
 {
     #[Route('/')]
     public function homepage(): Response
     {
         //die('Vinyl: Definitely NOT a fancy-looking frisbee!');
-        return new Response('Title: PB and Jams');
+        //return new Response('Title: PB and Jams');
+
+        $tracks = [
+        ['song' => 'Creep', 'artist' => 'Radiohead'],
+        ['song' => 'Live forever', 'artist' => 'Oasis'], 
+        ['song' => 'Live\'s an ocean', 'artist' => 'The Verve'], 
+        ['song' => 'Beetlebum', 'artist' => 'Blur'], 
+        ['song' => 'The Ressurection','artist' => 'The Stone Roses']];
+
+        return $this->render('vinyl/homepage.html.twig', ['title' => 'PB & Jams', 'tracks' => $tracks]);
     }
 
     #[Route('/browse/{slug}')]
